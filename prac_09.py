e5f96bda01ec8dd1a07f903a1c7dbe49d72aaf46
Ihor <berezjak@i.ua>
2020-06-15 12:53:23 +0300

    add file 9 Task2

--- prac_09.py
+++ prac_09.py
@@ -20,33 +20,99 @@
 # - для чтения из файла используем функцию файла "readlines" (возвращает лист строк файла)
 # - для удаление "\n" используем функцию "map"
 
-class Files:
-    def swap(self):
-        with open('file1.txt') as file1, open('file2.txt') as file2:
-            file1, file2 = file2, file1
-            f1 = file1.read().splitlines()
-            f2 = file2.read().splitlines()
-        return f'file1: {f1}, file2: {f2}'
-    def to_dict(self):
-        with open('file1.txt') as file1, open('file2.txt') as file2:
-            l1 = [row for row in file1.read().splitlines()]
-            l2 = [row for row in file2.read().splitlines()]
-            d1 = dict(zip(l1, l2))
-            d2 = dict(zip(l2, l1))
-        return f'dict1: {d1}; dict2: {d2}'
-f = Files().swap()
-print(f)
-dic = Files().to_dict()
-print(dic)
-def readlines():
-    with open('file1.txt') as file1, open('file2.txt') as file2:
-        # l1 = [row.rstrip('\n') for row in file1]
-        # l2 = [row.rstrip('\n') for row in file2]
-        l1 = list(map(lambda row: row.rstrip('\n'), file1))
-        l2 = list(map(lambda row: row.rstrip('\n'), file2))
-        print(l1)
-        print(l2)
-readlines()
+# class Files:
+#     def swap(self):
+#         with open('file1.txt') as file1, open('file2.txt') as file2:
+#             file1, file2 = file2, file1
+#             f1 = file1.read().splitlines()
+#             f2 = file2.read().splitlines()
+#         return f'file1: {f1}, file2: {f2}'
+#     def to_dict(self):
+#         with open('file1.txt') as file1, open('file2.txt') as file2:
+#             l1 = [row for row in file1.read().splitlines()]
+#             l2 = [row for row in file2.read().splitlines()]
+#             d = dict(zip(l2, l1))
+#         return d
+# f = Files().swap()
+# print(f)
+# dic = Files().to_dict()
+# print(dic)
+# def readlines():
+#     with open('file1.txt') as file1, open('file2.txt') as file2:
+#         # l1 = [row.rstrip('\n') for row in file1]
+#         # l2 = [row.rstrip('\n') for row in file2]
+#         l1 = list(map(lambda row: row.rstrip('\n'), file1))
+#         l2 = list(map(lambda row: row.rstrip('\n'), file2))
+#         print(l1)
+#         print(l2)
+# readlines()
+
+# 0) створити клас User, який в init приймає name i mail.
+# 1) створити клас Letter, який в init приймає text
+# 2) створити функцію send_letter яка створює об'єкт user і letter,
+# 3) створює файл (назва файла має виглядати так "{user.name}.txt" де user.name - це імя щойно створеного юзера),
+# 4) текст файла має записатися з об'єкта letter.text
+# 5) має бути глобальний словник users де ключами є об'єкт user а значенням об'єкт letter,
+# 6) добавлення в словник має відбуватися автоматично коли викликається функція send_letter
+# 7) зробити меню для цього всього:
+#   1) надіслати листа - send_letter
+#   2) переглянути всіх юзерів з словника - вивести список імен які є в словнику
+#   3) переглянути лист - запитати ім'я, якщо є в словнику вивести лист, якщо немає то сказати що немає такого
+#   4) вихід
+users = {}
+
+class User:
+    def __init__(self, name, mail):
+        self.name = name
+        self.mail = mail
+
+class Letter:
+    def __init__(self, text):
+        self.text = text
+
+def send_letter(name, mail, text):
+    user = User(name, mail)
+    letter = Letter(text)
+    with open(f"{user.name}.txt", 'w') as file:
+        file.write(letter.text)
+        users[user.name] = letter.text
 
 
+def search_name(arr, name):
+    hr()
+    check = True
+    for item in arr:
+        if name.lower().strip() == item.lower().strip():
+            print(arr.keys())
+            check = False
+    if check:
+        print("Name not found!!!")
+    hr()
 
+def hr():
+    print('------------------------------------------------------')
+
+while True:
+    print('1. Run send_letter')
+    print('2. Show all users; list name what is in dict')
+    print('3. Search name')
+    print('4. Exit')
+    choice = input('Make your choice: ')
+    print('')
+    if not choice.isdigit():
+        continue
+    choice = int(choice)
+    if choice == 1:
+        send_letter('Olga', 'yyy', 'Hello Brother')
+        send_letter('Alex', 'zzz', 'Hello Dad')
+        send_letter('Pit', 'xxx', 'Hello Mom')
+    if choice == 2:
+        hr()
+        print(users)
+        hr()
+        print(users.keys())
+        hr()
+    if choice == 3:
+        search_name(users, input('Enter name: '))
+    if choice == 4:
+        break
